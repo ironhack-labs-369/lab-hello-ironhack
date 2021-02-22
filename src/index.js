0fc9c5a7c0ca2aeb120369fa8cd756e6aa2e0e6e
mcapurri <m.capurri@gmail.com>
2021-02-22 13:03:09 +0100

    done

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const element = (
+    <>
+        <section>
+            <img src="" alt="" />
+        </section>
+        <section>
+            <article>
+                <img src="images/icon1" alt="icon1" />
+                <p>React makes it painless to create interactive UIs</p>
+            </article>
+            <article>
+                <img src="images/icon2" alt="icon2" />
+                <p>Build encapsulated components that manage their state</p>
+            </article>
+            <article>
+                <img src="images/icon3" alt="icon3" />
+                <p>A set of immutable values are passed to the components</p>
+            </article>
+            <article>
+                <img src="images/icon4" alt="icon4" />
+                <p>Staticall-typed designed to run on modern browsers </p>
+            </article>
+        </section>
+    </>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
